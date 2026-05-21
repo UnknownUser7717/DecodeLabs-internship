fbe4f5802b1618f5ab93255a50530985668dc62e
Donia Gamal <doniagamal2006@gmail.com>
2026-05-21 21:07:33 +0300

    QUERIES

--- DATABASE DRC .sql
+++ DATABASE DRC .sql
@@ -1,13 +1,52 @@
 USE DecodelabsDB;
 
 
-BULK INSERT Orders
-FROM 'D:\files\Code\DecodeLabs-internship\Data\cleaned_dataset.csv'
-WITH (
-    FIELDTERMINATOR = ',',
-    ROWTERMINATOR = '\n',
-    FIRSTROW = 2,
-    TABLOCK
-);
+-- BULK INSERT Orders
+-- FROM 'D:\files\Code\DecodeLabs-internship\Data\cleaned_dataset.csv'
+-- WITH (
+--     FIELDTERMINATOR = ',',
+--     ROWTERMINATOR = '\n',
+--     FIRSTROW = 2,
+--     TABLOCK
+-- );
 
 SELECT TOP 5 * FROM Orders;
+
+SELECT Product, ROUND(SUM(TotalPrice), 2) AS TotalSales
+FROM Orders
+GROUP BY Product
+ORDER BY TotalSales DESC;
+
+SELECT OrderStatus, COUNT(*) AS TotalOrders
+FROM Orders
+GROUP BY OrderStatus
+ORDER BY TotalOrders DESC;
+
+SELECT ReferralSource, ROUND(AVG(TotalPrice), 2) AS AvgOrderValue
+FROM Orders
+GROUP BY ReferralSource
+ORDER BY AvgOrderValue DESC;
+GO
+
+SELECT Product, ROUND(SUM(TotalPrice), 2) AS TotalSales
+FROM Orders
+GROUP BY Product
+HAVING SUM(TotalPrice) > 300000
+ORDER BY TotalSales DESC;
+
+SELECT PaymentMethod, ROUND(AVG(UnitPrice), 2) AS AvgUnitPrice
+FROM Orders
+GROUP BY PaymentMethod
+HAVING AVG(UnitPrice) > 350
+ORDER BY AvgUnitPrice DESC;
+
+SELECT Product, OrderStatus, COUNT(*) AS TotalOrders
+FROM Orders
+GROUP BY Product, OrderStatus
+ORDER BY Product ASC, TotalOrders DESC;
+
+SELECT TOP 3 PaymentMethod, ROUND(sum(TotalPrice), 2) AS TotalRevenue
+from Orders
+group by PaymentMethod
+having COUNT(*) > 200
+order by TotalRevenue desc;
